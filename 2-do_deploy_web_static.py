9dcb798ee9cc912b23b4eb2aa75cc13b52360b6b
theyasflower <84896487+theyasflower@users.noreply.github.com>
2023-07-06 00:38:28 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,52 +1,39 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to your web servers
-"""
-
-from datetime import datetime
-from fabric.api import *
+from fabric.api import run, local, put, env
+import datetime
 import os
-
-env.hosts = ["52.91.121.146", "3.85.136.181"]
-env.user = "ubuntu"
 
 
 def do_pack():
-    """
-        return the archive path if archive has generated correctly.
-    """
-
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archived_f_path = "versions/web_static_{}.tgz".format(date)
-    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
-
-    if t_gzip_archive.succeeded:
-        return archived_f_path
-    else:
+    now = datetime.datetime.now()
+    date = (str(now.year) + str(now.month) + str(now.day) + str(now.hour) +
+            str(now.minute) + str(now.second))
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz ./web_static".format(date))
+        return "/versions/web_static_{}".format(date)
+    except:
         return None
 
 
+env.hosts = ['35.190.188.208', '35.227.47.184']
+
+
 def do_deploy(archive_path):
-    """
-        Distribute archive.
-    """
-    if os.path.exists(archive_path):
-        archived_file = archive_path[9:]
-        newest_version = "/data/web_static/releases/" + archived_file[:-4]
-        archived_file = "/tmp/" + archived_file
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
-                                             newest_version))
-        run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
-                                                newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(newest_version))
-
-        print("New version deployed!")
+    if os.path.exists(archive_path) is False:
+        return False
+    try:
+        upload = put(archive_path, "/tmp/")
+        name = archive_path[9:-4]
+        rmt_path = "/data/web_static/releases/" + name
+        run("mkdir {}".format(rmt_path))
+        run("tar -xvzf /tmp/{}.tgz --directory {}/".format(name, rmt_path))
+        run("rm /tmp/{}.tgz".format(name))
+        run("rm /data/web_static/current")
+        run("ln -nsf /data/web_static/releases/{} /data/web_static/current"
+            .format(name))
+        run("mv {}/web_static/* {}".format(rmt_path, rmt_path))
+        run("rm -d {}/web_static/".format(rmt_path))
         return True
-
-    return False
+    except:
+        return False
